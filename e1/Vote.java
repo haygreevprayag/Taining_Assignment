03ca426eb81dd616098585895f5ff0103717630e
haygreevprayag <66084810+haygreevprayag@users.noreply.github.com>
2021-04-16 09:52:45 +0530

    Create Vote.java

---
+++ e1/Vote.java
@@ -0,0 +1,18 @@
+package com.oops.project;
+import java.util.*;
+class Vote
+{
+	public static void main(String args[]){
+		int age;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("What is your age?");
+		age=sc.nextInt();
+		if(age>=18)
+			System.out.println("You are eligible to vote.");
+		else
+			System.out.println("You are not eligible to vote.");
+
+		}
+}
+
+
